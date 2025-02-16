74fc1a070fdc3e91f4ff48329694299825199aae
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-16 16:06:23 +0200

    Add more crud functions

--- users/query.py
+++ users/query.py
@@ -4,6 +4,12 @@ from sqlalchemy.orm import Session
 
 from db import get_db
 from models import User
+from auth import hash_password
+from schema import UserCreate, UserUpdate
+
+
+async def is_user_exist(username: str, email: str, db: Session = Depends(get_db)) -> User:
+    return db.query(User).filter(User.username == username or User.email == email).first()
 
 
 async def get_user_by_id(id: int, db: Session = Depends(get_db)) -> User:
@@ -32,3 +38,44 @@ async def get_admin_users(db: Session = Depends(get_db)) -> List[User]:
 
 async def get_non_admin_users(db: Session = Depends(get_db)) -> List[User]:
     return db.query(User).filter(User.is_admin == False)
+
+
+def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone_number=user.phone_number,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+async def update_user(email: str, user: UserUpdate, db: Session = Depends(get_db)) -> User:
+    db_user = await get_user_by_email(email)
+
+    if user.first_name and db_user.first_name != user.first_name:
+        db_user.first_name = user.first_name
+
+    if user.last_name and db_user.last_name != user.last_name:
+        db_user.last_name = user.last_name
+
+    if user.phone_number and db_user.phone_number != user.phone_number:
+        db_user.phone_number = user.phone_number
+
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+async def delete_user(email: str, db: Session = Depends(get_db)) -> None:
+    db_user = await get_user_by_email(email)
+    db.delete(db_user)
+    db.commit()
