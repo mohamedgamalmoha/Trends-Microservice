9f87397723de5e603a515681fa56835ee712e1b3
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-18 20:01:33 +0200

    Add error messages

---
+++ users/messages.py
@@ -0,0 +1,5 @@
+
+INVALID_TOKEN_MESSAGE = 'Invalid Token'
+EXPIRED_TOKEN_MESSAGE = 'Token has expired'
+USER_ALREADY_EXIST_MESSAGE = 'User with that email already exists'
+USER_CREATED_MESSAGE = 'User Registered, Please verify email to activate account !'
