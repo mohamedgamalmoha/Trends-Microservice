c6ddf08a5d6e0573dd9001fe6a9af68a2c0894e9
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-18 20:02:05 +0200

    Add Token Exception

---
+++ users/exceptions.py
@@ -0,0 +1,13 @@
+import messages
+
+
+class TokenError(Exception):
+    ...
+
+
+class InvalidTokenError(TokenError):
+    message = messages.INVALID_TOKEN_MESSAGE
+
+
+class TokenExpiredError(TokenError):
+    message = messages.EXPIRED_TOKEN_MESSAGE
