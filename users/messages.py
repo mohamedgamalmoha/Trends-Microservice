64e23b51798117972cf4ad0b0ea56fbfd3597b5e
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-20 01:35:51 +0200

    Add user forbidden message

--- users/messages.py
+++ users/messages.py
@@ -4,4 +4,4 @@ EXPIRED_TOKEN_MESSAGE = 'Token has expired'
 INVALID_CREDENTIALS_MESSAGE = 'No user matches the given credentials'
 USER_ALREADY_EXIST_MESSAGE = 'User with that email already exists'
 USER_NOT_FOUND_MESSAGE = 'User not found'
-
+USER_FORBIDDEN_MESSAGE = 'You are not allowed to perform this action'
