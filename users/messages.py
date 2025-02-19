a3622e1fa92e92c1bda25d1af0837850aefd1b6c
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-19 10:59:00 +0200

    Add user related messages

--- users/messages.py
+++ users/messages.py
@@ -1,5 +1,7 @@
 
 INVALID_TOKEN_MESSAGE = 'Invalid Token'
 EXPIRED_TOKEN_MESSAGE = 'Token has expired'
+INVALID_CREDENTIALS_MESSAGE = 'No user matches the given credentials'
 USER_ALREADY_EXIST_MESSAGE = 'User with that email already exists'
-USER_CREATED_MESSAGE = 'User Registered, Please verify email to activate account !'
+USER_NOT_FOUND_MESSAGE = 'User not found'
+
