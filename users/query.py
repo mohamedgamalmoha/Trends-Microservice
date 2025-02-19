69bdee2f8c98c2deb16542ab3fd4d1771f398812
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-20 00:45:10 +0200

    Rewrite functions in async

--- users/query.py
+++ users/query.py
@@ -1,47 +1,68 @@
-from typing import List, Type
+from typing import Type, Sequence
 
 from fastapi import Depends
-from sqlalchemy.orm import Session
-from sqlalchemy.sql import exists, or_
+from sqlalchemy.sql import select, exists, or_
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_db
-from models import User
+from src.users.models import User
 
 
-def is_user_exist(email: str, username: str, db: Session = Depends(get_db)) -> bool:
-    return db.query(
-        exists().where(
-            or_(
-                User.email == email,
-                User.username == username
+async def is_user_exist(email: str, username: str, db: AsyncSession = Depends(get_db)) -> bool:
+    result = await db.execute(
+        select(
+            exists().where(
+                or_(
+                    User.email == email,
+                    User.username == username
+                )
             )
         )
-    ).scalar()
+    )
+    return result.scalar()
 
 
-def get_user_by_id(id: int, db: Session = Depends(get_db)) -> Type[User] | None:
-    return db.query(User).filter_by(id=id, is_active=True).first()
+async def get_user_by_id(id: int, db: AsyncSession = Depends(get_db)) -> Type[User] | None:
+    result = await db.execute(
+        select(User).filter_by(id=id, is_active=True)
+    )
+    return result.scalar_one_or_none()
 
 
-def get_user_by_email(email: str, db: Session = Depends(get_db)) -> Type[User] | None:
-    return db.query(User).filter_by(email=email, is_active=True).first()
+async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)) -> Type[User] | None:
+    result = await db.execute(
+        select(User).filter_by(email=email, is_active=True)
+    )
+    return result.scalar_one_or_none()
 
 
-def get_user_by_username(username: str, db: Session = Depends(get_db))-> Type[User] | None:
-    return db.query(User).filter_by(username=username, is_active=True).first()
+async def get_user_by_username(username: str, db: AsyncSession = Depends(get_db)) -> Type[User] | None:
+    result = await db.execute(
+        select(User).filter_by(username=username, is_active=True)
+    )
+    return result.scalar_one_or_none()
 
 
-def get_all_users(db: Session = Depends(get_db)) -> List[Type[User]]:
-    return db.query(User).all()
+async def get_all_users(db: AsyncSession = Depends(get_db)) -> Sequence[User]:
+    result = await db.execute(select(User))
+    return result.scalars().all()
 
 
-def get_active_users(db: Session = Depends(get_db)) -> List[Type[User]]:
-    return db.query(User).filter_by(is_active=True).all()
+async def get_active_users(db: AsyncSession = Depends(get_db)) -> Sequence[User]:
+    result = await db.execute(
+        select(User).filter_by(is_active=True)
+    )
+    return result.scalars().all()
+
+async def get_admin_users(db: AsyncSession = Depends(get_db)) -> Sequence[User]:
+    result = await db.execute(
+        select(User).filter_by(is_admin=True)
+    )
+    return result.scalars().all()
 
 
-def get_admin_users(db: Session = Depends(get_db)) -> List[Type[User]]:
-    return db.query(User).filter_by(is_admin=True).all()
-
-
-def get_non_admin_users(db: Session = Depends(get_db)) -> List[Type[User]]:
-    return db.query(User).filter_by(is_admin=False).all()
+async def get_non_admin_users(db: AsyncSession = Depends(get_db)) -> Sequence[User]:
+    result = await db.execute(
+        select(User).filter_by(is_admin=False)
+    )
+    return result.scalars().all()
--- users/repository.py
+++ users/repository.py
@@ -1,7 +1,7 @@
 from typing import Type
 
 from fastapi import Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_db
 from models import User
@@ -10,7 +10,7 @@ from query import get_user_by_id
 from schema import UserCreate, AdminUserCreate, UserUpdate
 
 
-def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)) -> User:
     db_user = User(
         username=user.username,
         email=user.email,
@@ -20,14 +20,15 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
         hashed_password=hash_password(user.password)
     )
 
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    async with db.begin():
+        db.add(db_user)
+        await db.commit()
+        await db.refresh(db_user)
 
     return db_user
 
 
-def create_admin_user(user: AdminUserCreate, db: Session = Depends(get_db)) -> User:
+async def create_admin_user(user: AdminUserCreate, db: AsyncSession = Depends(get_db)) -> User:
     db_user = User(
         username=user.username,
         email=user.email,
@@ -36,15 +37,19 @@ def create_admin_user(user: AdminUserCreate, db: Session = Depends(get_db)) -> U
         hashed_password=hash_password(user.password)
     )
 
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    async with db.begin():
+        db.add(db_user)
+        await db.commit()
+        await db.refresh(db_user)
 
     return db_user
 
 
-def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)) -> Type[User] | None:
-    db_user = get_user_by_id(user_id)
+async def update_user(user_id: int, user: UserUpdate, db: AsyncSession = Depends(get_db)) -> Type[User] | None:
+    db_user = await get_user_by_id(user_id, db)
+
+    if not db_user:
+        return None
 
     if user.first_name and db_user.first_name != user.first_name:
         db_user.first_name = user.first_name
@@ -55,13 +60,19 @@ def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)) -
     if user.phone_number and db_user.phone_number != user.phone_number:
         db_user.phone_number = user.phone_number
 
-    db.commit()
-    db.refresh(db_user)
+    async with db.begin():
+        await db.commit()
+        await db.refresh(db_user)
 
     return db_user
 
 
-def delete_user(user_id: int, db: Session = Depends(get_db)) -> None:
-    db_user = get_user_by_id(user_id)
-    db.delete(db_user)
-    db.commit()
+async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)) -> None:
+    db_user = await get_user_by_id(user_id, db)
+
+    if not db_user:
+        return None
+
+    async with db.begin():
+        await db.delete(db_user)
+        await db.commit()
