b25333b203b270578c67a346113afa97eacb520a
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-20 18:06:28 +0200

    Use api router

--- users/main.py
+++ users/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
 from db import lifespan
+from routes import auth_router, user_router
 
 
 app = FastAPI(lifespan=lifespan)
 
-
-from routes import *
+app.include_router(auth_router, prefix='/api')
+app.include_router(user_router, prefix='/api')
--- users/routes.py
+++ users/routes.py
@@ -1,10 +1,9 @@
 from typing import List
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 import messages
-from main import app
 from db import get_db
 from models import User
 from auth import authenticate_user, create_access_token, get_current_user
@@ -13,7 +12,18 @@ from repository import create_user, update_user, delete_user
 from query import is_user_exist,  get_user_by_id, get_all_users
 
 
-@app.post('/api/jwt/create/', status_code=status.HTTP_200_OK, response_model=Token)
+auth_router = APIRouter(
+    prefix='/jwt',
+    tags = ['auth']
+)
+
+user_router = APIRouter(
+    prefix='/users',
+    tags = ['users']
+)
+
+
+@auth_router.post('/create/', status_code=status.HTTP_200_OK, response_model=Token)
 async def create_jwt_route(user_data: UserLogin, db: AsyncSession = Depends(get_db)):
     user = await authenticate_user(email=user_data.email, password=user_data.password, db=db)
 
@@ -28,12 +38,12 @@ async def create_jwt_route(user_data: UserLogin, db: AsyncSession = Depends(get_
     return Token(access_token=access_token)
 
 
-@app.get('/api/users/me/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
+@user_router.get('/me/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
 async def get_user_route(current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
     return current_user
 
 
-@app.post('/api/users/', status_code=status.HTTP_201_CREATED, response_model=UserRetrieve)
+@user_router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserRetrieve)
 async def create_user_route(user_data: UserCreate,  db: AsyncSession = Depends(get_db)):
     db_user = await is_user_exist(username=user_data.username, email=user_data.email, db=db)
 
@@ -48,7 +58,7 @@ async def create_user_route(user_data: UserCreate,  db: AsyncSession = Depends(g
     return db_user
 
 
-@app.get('/api/users/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
+@user_router.get('/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
 async def get_user_route(user_id: int, current_user: User = Depends(get_current_user),
                          db: AsyncSession = Depends(get_db)):
     user = await get_user_by_id(id=user_id, db=db)
@@ -68,7 +78,7 @@ async def get_user_route(user_id: int, current_user: User = Depends(get_current_
     )
 
 
-@app.get('/api/users/', status_code=status.HTTP_200_OK, response_model=List[UserRetrieve])
+@user_router.get('/', status_code=status.HTTP_200_OK, response_model=List[UserRetrieve])
 async def get_users_route(current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
 
     if current_user.is_admin:
@@ -81,7 +91,7 @@ async def get_users_route(current_user: User = Depends(get_current_user), db: As
     )
 
 
-@app.delete('/api/users/{user_id}/', status_code=status.HTTP_204_NO_CONTENT)
+@user_router.delete('/{user_id}/', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user_route(user_id: int, current_user: User = Depends(get_current_user),
                             db: AsyncSession = Depends(get_db)):
     user = await get_user_by_id(id=user_id, db=db)
@@ -102,7 +112,7 @@ async def delete_user_route(user_id: int, current_user: User = Depends(get_curre
     )
 
 
-@app.put('/api/users/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
+@user_router.put('/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
 async def update_user_route(user_id: int, user_data: UserUpdate, current_user: User = Depends(get_current_user),
                       db: AsyncSession = Depends(get_db)):
     user = await get_user_by_id(id=user_id, db=db)
