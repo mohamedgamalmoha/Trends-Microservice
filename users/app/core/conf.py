c787da5eaf5d2f224e5418e421986c90dae63350
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-25 22:45:21 +0200

    Add more tokens conf

--- users/app/core/conf.py
+++ users/app/core/conf.py
@@ -11,11 +11,26 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Settings(BaseSettings):
+    # General
     SQLALCHEMY_DATABASE_URL: str= os.environ.get('SQLALCHEMY_DATABASE_URL')
-    SECRET_KEY: str = os.environ.get("SECRET_KEY")
+
+    # Password Envs
+    PASSWORD_CRYPT_CONTEXT_SCHEMA: str = os.environ.get("PASSWORD_CRYPT_CONTEXT_SCHEMA")
+
+    # Access Token Envs
+    ACCESS_TOKEN_SECRET_KEY: str = os.environ.get("ACCESS_TOKEN_SECRET_KEY")
     ACCESS_TOKEN_ALGORITHM: str = os.environ.get("ACCESS_TOKEN_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
-    PASSWORD_CRYPT_CONTEXT_SCHEMA: str = os.environ.get("PASSWORD_CRYPT_CONTEXT_SCHEMA")
+
+    # Verification Token Envs
+    VERIFICATION_TOKEN_SECRET_KEY: str = os.environ.get("VERIFICATION_TOKEN_SECRET_KEY")
+    VERIFICATION_TOKEN_ALGORITHM: str = os.environ.get("VERIFICATION_TOKEN_ALGORITHM")
+    VERIFICATION_TOKEN_EXPIRE_MINUTES: int = os.environ.get("VERIFICATION_TOKEN_EXPIRE_MINUTES")
+
+    # Password Reset Token Envs
+    PASSWORD_REST_TOKEN_SECRET_KEY: str = os.environ.get("PASSWORD_REST_TOKEN_SECRET_KEY")
+    PASSWORD_REST_TOKEN_ALGORITHM: str = os.environ.get("PASSWORD_REST_TOKEN_ALGORITHM")
+    PASSWORD_REST_TOKEN_EXPIRE_MINUTES: int = os.environ.get("PASSWORD_REST_TOKEN_EXPIRE_MINUTES")
 
 
 settings = Settings()
