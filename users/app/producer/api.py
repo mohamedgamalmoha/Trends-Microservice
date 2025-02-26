e5e7fcc13e469b63c1201bb961ae33184d85f651
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-26 02:32:06 +0200

    Add schema to functions

--- users/app/producer/api.py
+++ users/app/producer/api.py
@@ -1,7 +1,8 @@
 from app.utils import safe_call
-from app.schemas.user import UserRetrieve
-from app.producer.producer import UserMessageProducer
 from app.producer.conf import settings, USER_RABBITMQ_EXCHANGE, USER_RABBITMQ_QUEUES
+from app.schemas.producer import (UserCreationProducerMessage, UserEmailVerificationProducerMessage,
+                                  UserResetPasswordProducerMessage)
+from app.producer.producer import UserMessageProducer
 
 
 def get_producer() -> UserMessageProducer:
@@ -18,21 +19,21 @@ async def init_producer() -> None:
     await producer.init_user_producer()
 
 
-async def send_user_creation_message(user_data: UserRetrieve) -> None:
+async def send_user_creation_message(user_data: UserCreationProducerMessage) -> None:
     producer =  get_producer()
     await producer.send_user_creation_message(
         message_data=user_data.model_dump()
     )
 
 
-async def send_user_email_verification_message(user_data: UserRetrieve) -> None:
+async def send_user_email_verification_message(user_data: UserEmailVerificationProducerMessage) -> None:
     producer = get_producer()
     await producer.send_user_email_verification_message(
         message_data=user_data.model_dump()
     )
 
 
-async def send_user_password_forget_message(user_data: UserRetrieve) -> None:
+async def send_user_password_forget_message(user_data: UserResetPasswordProducerMessage) -> None:
     producer = get_producer()
     await producer.send_user_password_forget_message(
         message_data=user_data.model_dump()
