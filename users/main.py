8d2dad75862ba11975a0c72a08f8b37fe0d5c70b
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-26 19:07:56 +0200

    Add more endpoints

--- users/main.py
+++ users/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.core.lifespan import lifespan
-from app.api.endpoints import auth, users
+from app.api.endpoints import auth, users, verification, password
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.auth_router, prefix='/api')
 app.include_router(users.user_router, prefix='/api')
+app.include_router(verification.email_verification_router, prefix='/api')
+app.include_router(password.password_reset_router, prefix='/api')
