95c33e52319d1b1f984e65830300fed25cf0c252
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-02-26 19:07:04 +0200

    Enhance password schemas

--- users/app/schemas/password.py
+++ users/app/schemas/password.py
@@ -6,4 +6,12 @@ class UserPasswordReset(pydantic.BaseModel):
 
 
 class UsePasswordResetConfirmation(pydantic.BaseModel):
+    new_password: str
+    new_password_confirm: str
     reset_token: str
+
+    @pydantic.model_validator(mode='after')
+    def verify_password_match(self):
+        if self.new_password != self.new_password_confirm:
+            raise ValueError("The two passwords did not match.")
+        return self
