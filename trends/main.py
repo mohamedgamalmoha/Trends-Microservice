5d553ac4eb2d68750e7708b0c195d9d0f14db6e5
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-11 00:22:36 +0200

    Adjust docs urls

--- trends/main.py
+++ trends/main.py
@@ -3,6 +3,11 @@ from app.core.lifespan import lifespan
 from app.api.endpoints import task
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url='/api/tasks/docs',
+    redoc_url='/api/tasks/redoc',
+    openapi_url='/api/tasks/openapi.json'
+)
 
 app.include_router(task.task_router, prefix='/api')
--- users/main.py
+++ users/main.py
@@ -3,7 +3,12 @@ from app.core.lifespan import lifespan
 from app.api.endpoints import auth, users, verification, password
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url='/api/users/docs',
+    redoc_url='/api/users/redoc',
+    openapi_url='/api/users/openapi.json'
+)
 
 app.include_router(auth.auth_router, prefix='/api')
 app.include_router(users.user_router, prefix='/api')
