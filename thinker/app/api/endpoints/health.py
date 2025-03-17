7df5bc5affd0f3422c5bc83aca695188558b6e38
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-17 08:15:11 +0200

    Restructure health schema & endpoint

--- thinker/app/api/endpoints/health.py
+++ thinker/app/api/endpoints/health.py
@@ -1,8 +1,9 @@
 import httpx
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
+from app import messages
 from app.core.conf import settings
-from app.schemas.health import Health, HealthStatusEnum
+from app.schemas.health import Health
 
 
 health_router = APIRouter(
@@ -18,23 +19,21 @@ async def health_check():
             response = await client.get(f"{settings.OLLAMA_API_URL}/tags")
             if response.status_code == 200:
                 models = response.json().get("models", [])
-                print(models)
                 model_exists = any(model["name"] == settings.OLLAMA_MODEL_NAME for model in models)
                 if not model_exists:
-                    return {
-                        "status": HealthStatusEnum.WARNING,
-                        "message": f"Ollama is running but {settings.OLLAMA_MODEL_NAME} is not loaded."
-                    }
-                return {
-                    "status": HealthStatusEnum.OK,
-                    "message": "Ollama service is running with Deep Seek model loaded"
-                }
-            return {
-                "status": HealthStatusEnum.ERROR,
-                "message": "Ollama service returned unexpected response"
-            }
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail=messages.OLLAMA_MODEL_IS_NOT_LOADED
+                    )
+                return Health(
+                    message=messages.OLLAMA_SERVICE_IS_RUNNING
+                )
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=messages.OLLAMA_SERVICE_RETURNED_UNEXPECTED_RESPONSE
+            )
     except httpx.RequestError:
-        return {
-            "status": HealthStatusEnum.ERROR,
-            "message": "Cannot connect to Ollama service"
-        }
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=messages.CANNOT_CONNECT_TO_OLLAMA_SERVICE
+        )
--- thinker/app/schemas/health.py
+++ thinker/app/schemas/health.py
@@ -1,14 +1,5 @@
-import enum
-
 import pydantic
 
 
-class HealthStatusEnum(enum.Enum):
-    OK = "Ok"
-    ERROR = "Error"
-    WARNING = "Warning"
-
-
 class Health(pydantic.BaseModel):
-    status: HealthStatusEnum
     message: str
