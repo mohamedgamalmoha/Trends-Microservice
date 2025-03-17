182d78babb324fb98a9e38ddb7a0197b5384a24f
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-17 09:40:51 +0200

    Add sql link to settings

--- thinker/app/core/conf.py
+++ thinker/app/core/conf.py
@@ -8,6 +8,7 @@ dotenv.load_dotenv('../../.env')
 
 
 class Settings(BaseSettings):
+    SQLALCHEMY_DATABASE_URL: str = os.environ.get('SQLALCHEMY_DATABASE_URL')
     OLLAMA_API_URL: str = os.environ.get('OLLAMA_API_URL')
     OLLAMA_MODEL_NAME: str = os.environ.get('OLLAMA_MODEL_NAME')
     OLLAMA_REQUEST_TIMEOUT: int = os.environ.get('OLLAMA_REQUEST_TIMEOUT')
