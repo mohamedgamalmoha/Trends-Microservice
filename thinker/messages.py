7c155af0dceb390146b5a63a2f6041cf3fa8330f
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-17 08:14:57 +0200

    Add messages

---
+++ thinker/messages.py
@@ -0,0 +1,4 @@
+OLLAMA_MODEL_IS_NOT_LOADED = "Ollama is running but the Deep Seek model is not loaded."
+OLLAMA_SERVICE_IS_RUNNING = "Ollama service is running with Deep Seek model loaded"
+OLLAMA_SERVICE_RETURNED_UNEXPECTED_RESPONSE = "Ollama service returned unexpected response"
+CANNOT_CONNECT_TO_OLLAMA_SERVICE = "Cannot connect to Ollama service"
