607e94b9163b39704200d7d73dcfc7276ec867fc
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-03-17 23:58:57 +0000

    Add user auth/info url

--- thinker/app/core/conf.py
+++ thinker/app/core/conf.py
@@ -9,6 +9,10 @@ dotenv.load_dotenv('../../.env')
 
 class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str = os.environ.get('SQLALCHEMY_DATABASE_URL')
+   
+    USER_AUTH_URL: str = os.environ.get('USER_AUTH_URL')
+    USER_INFO_URL: str = os.environ.get('USER_INFO_URL')
+   
     OLLAMA_API_URL: str = os.environ.get('OLLAMA_API_URL')
     OLLAMA_MODEL_NAME: str = os.environ.get('OLLAMA_MODEL_NAME')
     OLLAMA_REQUEST_TIMEOUT: int = os.environ.get('OLLAMA_REQUEST_TIMEOUT')
