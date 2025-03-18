f6c1cca187af772ba867511459a5fa70b662f4b6
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-18 17:19:17 +0200

    Add more routes

--- trends/app/api/endpoints/task.py
+++ trends/app/api/endpoints/task.py
@@ -6,11 +6,11 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from app import messages
 from app.db.session import get_db
-from app.repositories.task import create_task, get_user_task_by_id, get_user_tasks
+from app.repositories.task import create_task, get_user_task_by_id, get_user_tasks, get_all_tasks, delete_task
 from app.schemas.user import User
 from app.schemas.task import TaskCreate, TaskRetrieve
 from app.celery.tasks import trends_search_task
-from app.api.deps import get_current_user
+from app.api.deps import get_current_user, get_current_admin_user
 
 
 task_router = APIRouter(
@@ -86,3 +86,45 @@ async def get_user_tasks_route(
         )
 
     return await get_user_tasks(user_id=user_id, db=db)
+
+
+@task_router.get("/tasks/", response_model=List[TaskRetrieve])
+async def get_tasks_route(
+        current_user: User = Depends(get_current_admin_user),
+        db: AsyncSession = Depends(get_db)
+    ):
+
+    db_tasks = await get_all_tasks(db=db)
+
+    return db_tasks
+
+
+@task_router.delete("/{user_id}/task/{task_id}/", response_model=None, status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task_route(
+        user_id: int,
+        task_id: str,
+        current_user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+    ):
+
+    if not current_user.is_admin and user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=messages.USER_FORBIDDEN_MESSAGE
+        )
+
+    db_task = await get_user_task_by_id(
+        user_id=user_id,
+        task_id=task_id,
+        db=db
+    )
+
+    if db_task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=messages.TASK_NOT_FOUND_MESSAGE
+        )
+
+    await delete_task(task_id=task_id, db=db)
+
+    return None
