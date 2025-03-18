073f3e11d3db9e73db02b936f3dcc94af80bda9f
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-19 00:27:46 +0200

    Add more messages

--- thinker/app/messages.py
+++ thinker/app/messages.py
@@ -3,3 +3,5 @@ OLLAMA_SERVICE_IS_RUNNING = "Ollama service is running with Deep Seek model load
 OLLAMA_SERVICE_RETURNED_UNEXPECTED_RESPONSE = "Ollama service returned unexpected response"
 CANNOT_CONNECT_TO_OLLAMA_SERVICE = "Cannot connect to Ollama service"
 USER_FORBIDDEN_MESSAGE = 'You are not allowed to perform this action'
+TASK_NOT_FOUND_MESSAGE = 'Task not found'
+SEARCH_TASKS_NOT_FOUND_MESSAGE = 'No tasks matching the given search task id'
