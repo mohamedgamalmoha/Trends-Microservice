362ee21bef88fc528c57365cfbe5252458085576
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-03-18 00:13:19 +0000

    Rewrite task route

--- thinker/app/api/endpoints/task.py
+++ thinker/app/api/endpoints/task.py
@@ -1,10 +1,14 @@
 from celery import uuid
-from fastapi import APIRouter, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, HTTPException, Depends, status
 
-from app.core.conf import settings
-from app.models.task import TaskStatus
-from app.schemas.task import TaskCreate, TaskRetrieve, ThinkResponse
+from app import messages
+from app.db.session import get_db
+from app.repositories.task import create_task
+from app.schemas.user import User
+from app.schemas.task import TaskCreate, TaskRetrieve
 from app.celery.tasks import think_task
+from app.api.deps import get_current_user
 
 
 task_router = APIRouter(
@@ -13,28 +17,34 @@ task_router = APIRouter(
 
 
 @task_router.post("/", response_model=TaskRetrieve)
-async def create_task_route(task: TaskCreate):
-    task_id = uuid()
-    user_id = 0
-    search_task_id = 0
-    results = await think_task(
-        question=task.question,
-        context=task.context,
-        temperature=task.temperature,
-        max_tokens=task.max_tokens
-    )
-    return TaskRetrieve(
-        task_id=task_id,
-        user_id=user_id,
-        search_task_id=search_task_id,
-        question=task.question,
-        context=task.context,
-        temperature=task.temperature,
-        max_tokens=task.max_tokens,
-        schedule_at=task.schedule_at,
-        status=TaskStatus.COMPLETED,
-        result_data=ThinkResponse(
-            answer=results['answer'],
-            thinking=results['thinking']
+async def create_task_route(
+        task: TaskCreate,
+        current_user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+    ):
+    
+    if not current_user.is_admin and task.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=messages.USER_FORBIDDEN_MESSAGE
         )
+    
+    task_id = uuid()
+
+    db_task = await create_task(
+        task_id=task_id,
+        task=task,
+        db=db
     )
+    
+    think_task.apply_sync(
+        kwargs={
+            "question": task.question,
+            "context": task.context,
+            "temperature": task.temperature,
+            "max_tokens": task.max_tokens
+        },
+        task_id=task_id
+    )
+
+    return db_task
