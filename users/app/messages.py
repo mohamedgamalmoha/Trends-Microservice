e9cf7fec17b28c96e203f4237b27d9bb7bd96ee9
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-20 11:12:45 +0200

    Remove unused files

--- users/app/messages.py
+++
@@ -1,7 +0,0 @@
-INVALID_TOKEN_MESSAGE = 'Invalid Token'
-EXPIRED_TOKEN_MESSAGE = 'Token has expired'
-INVALID_CREDENTIALS_MESSAGE = 'No user matches the given credentials'
-USER_ALREADY_EXIST_MESSAGE = 'User with that email already exists'
-USER_ALREADY_ACTIVE = 'This email is already verified.'
-USER_NOT_FOUND_MESSAGE = 'User not found'
-USER_FORBIDDEN_MESSAGE = 'You are not allowed to perform this action'
