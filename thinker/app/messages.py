b3ea9196612c365406840cac44e14769b226e0d1
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-20 07:25:05 +0200

    Remove messages file

--- thinker/app/messages.py
+++
@@ -1,7 +0,0 @@
-OLLAMA_MODEL_IS_NOT_LOADED = "Ollama is running but the Deep Seek model is not loaded."
-OLLAMA_SERVICE_IS_RUNNING = "Ollama service is running with Deep Seek model loaded"
-OLLAMA_SERVICE_RETURNED_UNEXPECTED_RESPONSE = "Ollama service returned unexpected response"
-CANNOT_CONNECT_TO_OLLAMA_SERVICE = "Cannot connect to Ollama service"
-USER_FORBIDDEN_MESSAGE = 'You are not allowed to perform this action'
-TASK_NOT_FOUND_MESSAGE = 'Task not found'
-SEARCH_TASKS_NOT_FOUND_MESSAGE = 'No tasks matching the given search task id'
