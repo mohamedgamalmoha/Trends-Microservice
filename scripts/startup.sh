39256ca40fe2d97b92d58e1788757fb944da7a2b
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-03-21 16:00:13 +0000

    Write startup script

---
+++ scripts/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Wait for database using pg_isready
+echo "Waiting for database..."
+until pg_isready -h users-db-serv -p 5432 -U users_db_user; do
+  echo "Database is unavailable - sleeping"
+  sleep 1
+done
+echo "Database is ready!"
+
+# Run migrations
+echo "Creating migrations..."
+alembic -c alembic.ini revision --autogenerate
+
+echo "Running migrations..."
+alembic -c alembic.ini upgrade head
+
+# Start the app
+echo "Starting the app..."
+uvicorn main:app --host 0.0.0.0 --port 80 --reload --http httptools --loop uvloop
