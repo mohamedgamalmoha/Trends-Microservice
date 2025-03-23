fd37f4e2028497b225c3b54e1de87b6e069398ff
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-23 15:08:05 +0200

    Rewrite bas task class

--- trends/app/celery/base_task.py
+++ trends/app/celery/base_task.py
@@ -1,5 +1,4 @@
 import traceback
-from datetime import datetime
 
 from celery import Task
 from pytrends.request import TrendReq
@@ -8,59 +7,63 @@ from shared_utils.schemas.status import TaskStatus
 from shared_utils.async_handler import AsyncHandler
 
 from app.repositories.task import update_task
-from app.schemas.task import TaskUpdate
+from app.schemas.task import TaskUpdate, TrendResponse, TrendError
 
 
 class TrendTask(Task):
 
+    @staticmethod
     @AsyncHandler.with_async_generator(get_db)
-    async def on_success(self, db, retval, task_id, args, kwargs):
+    async def before_start(db, task_id, args, kwargs):
+        await update_task(
+            task_id=task_id,
+            task_update=TrendResponse(
+                status=TaskStatus.IN_PROGRESS
+            ),
+            db=db
+        )
+
+    @staticmethod
+    @AsyncHandler.with_async_generator(get_db)
+    async def on_success(db, retval, task_id, args, kwargs):
         await update_task(
             task_id=task_id,
             task_update=TaskUpdate(
                 status=TaskStatus.COMPLETED,
-                result_data=retval,
-                updated_at=datetime.now()
+                result_data=TrendResponse(
+                    ** retval
+                )
             ),
             db=db
         )
 
+    @staticmethod
     @AsyncHandler.with_async_generator(get_db)
-    async def after_return(self, db, status, retval, task_id, args, kwargs, einfo):
+    async def on_failure(db, exc, task_id, args, kwargs, einfo):
         await update_task(
             task_id=task_id,
-            task_update=TaskUpdate(
-                updated_at=datetime.now()
-            ),
-            db=db
-        )
-
-    @AsyncHandler.with_async_generator(get_db)
-    async def on_retry(self, db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=TaskUpdate(
-                status=TaskStatus.IN_PROGRESS,
-                error_message=str(exc),
-                updated_at=datetime.now()
-            ),
-            increment=1,
-            db=db
-        )
-
-    @AsyncHandler.with_async_generator(get_db)
-    async def on_failure(self, db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=TaskUpdate(
+            task_update=TrendResponse(
                 status=TaskStatus.FAILED,
-                error_message=str(exc),
-                result_data={
-                    'error_type': type(exc).__name__,
-                    'error_details': str(exc),
-                    'traceback': traceback.format_exc()
-                },
-                updated_at=datetime.now()
+                error=TrendError(
+                    code=500,
+                    error=traceback.format_exc()
+                )
+            ),
+            db=db
+        )
+
+    @staticmethod
+    @AsyncHandler.with_async_generator(get_db)
+    async def on_retry(db, exc, task_id, args, kwargs, einfo):
+        await update_task(
+            task_id=task_id,
+            task_update=TrendResponse(
+                status=TaskStatus.RETRY,
+                error=TrendError(
+                    code=500,
+                    error=traceback.format_exc()
+                ),
+                increment_retry_count=True
             ),
             db=db
         )
