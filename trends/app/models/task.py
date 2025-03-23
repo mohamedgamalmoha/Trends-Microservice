4d050556fc1ead4606a49ac80f726d889422f075
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-23 14:36:07 +0200

    Adjust task model

--- trends/app/models/task.py
+++ trends/app/models/task.py
@@ -1,3 +1,4 @@
+import enum
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime, JSON, Enum
@@ -5,17 +6,29 @@ from shared_utils.db.base import Base
 from shared_utils.schemas.status import TaskStatus
 
 
+class PropertyEnum(enum.Enum):
+    WEB_SEARCH = "web"
+    YOUTUBE_SEARCH = "youtube"
+    NEWS_SEARCH = "news"
+    IMAGE_SEARCH = "images"
+    FROOGLE_SEARCH = "froogle"
+
+
 class Task(Base):
     __tablename__ = 'tasks'
 
     task_id = Column(String, index=True, primary_key=True)
     user_id = Column(Integer, index=True)
-    status = Column(Enum(TaskStatus), default=TaskStatus.PENDING)
-    request_data = Column(JSON)
+    geo = Column(JSON)
+    time = Column(String, nullable=True)
+    cat = Column(Integer, default=0)
+    gprop = Column(Enum(), default=PropertyEnum.WEB_SEARCH)
+    tz = Column(Integer, default=0)
     schedule_at = Column(DateTime, default=datetime.now)
 
+    status = Column(Enum(TaskStatus), default=TaskStatus.PENDING)
     result_data = Column(JSON, nullable=True)
-    error_message = Column(String, nullable=True)
+    error = Column(String, nullable=True)
     retry_count = Column(Integer, default=0)
 
     created_at = Column(DateTime, default=datetime.now)
