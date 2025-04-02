2a98a312f1e08042ba3cf01e4ef3b5ac2d3854a7
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-02 14:09:12 +0200

    Write essential test fixtures

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -1,6 +1,105 @@
 import dotenv
+import pytest
+import pytest_asyncio
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from testcontainers.postgres import PostgresContainer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
 
 dotenv.load_dotenv('.env.test')
 
+Base = declarative_base()
 
-from app.core.conf import settings
+
+@pytest.fixture(scope="module")
+def postgres_container():
+    # Start a PostgreSQL container with Testcontainers
+    with PostgresContainer(
+            image="postgres:17.4-alpine",
+            username='users_test_db_user',
+            password='123456',
+            dbname='users_test_db',
+            port=5500,
+            driver="asyncpg"
+        ).with_exposed_ports(
+            5555
+        ).with_bind_ports(
+        5555,
+        5555
+        ) as postgres:
+
+        yield postgres
+
+
+@pytest_asyncio.fixture(scope='module')
+async def engine(postgres_container):
+
+    engine = create_async_engine(
+        url=postgres_container.get_connection_url()
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(
+            Base.metadata.create_all
+        )
+
+    yield engine
+
+    async with engine.begin() as conn:
+        await conn.run_sync(
+            Base.metadata.drop_all
+        )
+
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="module")
+async def session_local(engine):
+    session_local = async_sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autocommit=False
+    )
+    return session_local
+
+
+@pytest_asyncio.fixture(scope='function')
+async def get_db(session_local):
+    async with session_local() as session:
+        yield session
+        await session.rollback()  # Ensure clean state between tests
+
+
+@pytest.fixture(scope="module")
+def app(postgres_container, get_db):
+    from shared_utils.core.conf import settings
+
+    settings.SQLALCHEMY_DATABASE_URL = postgres_container.get_connection_url()
+
+    from shared_utils.db.session import get_db as default_get_db
+    from app.api.endpoints import auth, users, verification, password
+
+
+    test_app = FastAPI(
+        docs_url='/api/users/docs',
+        redoc_url='/api/users/redoc',
+        openapi_url='/api/users/openapi.json'
+    )
+
+    test_app.include_router(auth.auth_router, prefix='/api')
+    test_app.include_router(users.user_router, prefix='/api')
+    test_app.include_router(verification.email_verification_router, prefix='/api')
+    test_app.include_router(password.password_reset_router, prefix='/api')
+
+    test_app.dependency_overrides[default_get_db] = get_db
+
+    return test_app
+
+
+@pytest.fixture(scope="module")
+def client(app):
+    with TestClient(app) as client:
+        yield client
