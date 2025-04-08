335f7c46d0a4e9844cb861bf0f9036a582f0e3c3
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-08 16:32:25 +0200

    Use dependency injection for producer

--- users/app/api/endpoints/verification.py
+++ users/app/api/endpoints/verification.py
@@ -9,7 +9,8 @@ from app.repositories.user import get_user_by_email, activate_user
 from app.schemas.verification import UserEmailVerification, UserEmailVerificationConfirmation
 from app.schemas.producer import UserEmailVerificationProducerMessage
 from app.utils import db_model_to_dict
-from app.producer.api import send_user_email_verification_message
+from app.producer.api import get_producer
+from app.producer.producer import UserMessageProducer
 from app.api.deps import create_email_verification_token, decode_email_verification_token
 
 
@@ -20,7 +21,12 @@ email_verification_router = APIRouter(
 
 
 @email_verification_router.post("/", status_code=status.HTTP_204_NO_CONTENT)
-async def send_email_verification(user_data: UserEmailVerification, db: AsyncSession = Depends(get_db)):
+async def send_email_verification(
+        user_data: UserEmailVerification,
+        db: AsyncSession = Depends(get_db),
+        producer: UserMessageProducer = Depends(get_producer)
+    ):
+
     db_user = await get_user_by_email(email=user_data.email, db=db)
     if not db_user:
         raise HTTPException(
@@ -43,11 +49,17 @@ async def send_email_verification(user_data: UserEmailVerification, db: AsyncSes
         verification_token=verification_token
     )
 
-    await send_user_email_verification_message(user_data=user)
+    await producer.send_user_email_verification_message(
+        user_data=user.model_dump()
+    )
 
 
 @email_verification_router.post("/confirm/", status_code=status.HTTP_204_NO_CONTENT)
-async def confirm_email_verification(user_data: UserEmailVerificationConfirmation, db: AsyncSession = Depends(get_db)):
+async def confirm_email_verification(
+        user_data: UserEmailVerificationConfirmation,
+        db: AsyncSession = Depends(get_db)
+    ):
+
     pyload =  decode_email_verification_token(token=user_data.verification_token)
     email = pyload.get('email', None)
 
