542ba39cb5fdf4929241b05d72baa1ebe757a4b7
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-08 16:28:25 +0200

    Use dependency injection for producer

--- users/app/api/endpoints/password.py
+++ users/app/api/endpoints/password.py
@@ -9,7 +9,8 @@ from app.repositories.user import get_user_by_email, reset_user_password
 from app.schemas.password import UserPasswordReset, UsePasswordResetConfirmation
 from app.schemas.producer import UserResetPasswordProducerMessage, UserResetPasswordConfirmationProducerMessage
 from app.utils import db_model_to_dict
-from app.producer.api import send_user_email_verification_message
+from app.producer.api import get_producer
+from app.producer.producer import UserMessageProducer
 from app.api.deps import create_password_reset_token, decode_password_reset_token
 
 
@@ -20,7 +21,12 @@ password_reset_router = APIRouter(
 
 
 @password_reset_router.post("/", status_code=status.HTTP_204_NO_CONTENT)
-async def request_password_reset(user_data: UserPasswordReset, db: AsyncSession = Depends(get_db)):
+async def request_password_reset(
+        user_data: UserPasswordReset,
+        db: AsyncSession = Depends(get_db),
+        producer: UserMessageProducer = Depends(get_producer)
+    ):
+
     db_user = await get_user_by_email(email=user_data.email, db=db)
     if not db_user:
         raise HTTPException(
@@ -37,11 +43,17 @@ async def request_password_reset(user_data: UserPasswordReset, db: AsyncSession 
         reset_token=reset_token
     )
 
-    await send_user_email_verification_message(user_data=user)
+    await producer.send_user_email_verification_message(
+        user_data=user.model_dump()
+    )
 
 
 @password_reset_router.post("/confirm/", status_code=status.HTTP_200_OK)
-async def confirm_password_reset(user_data: UsePasswordResetConfirmation, db: AsyncSession = Depends(get_db)):
+async def confirm_password_reset(
+        user_data: UsePasswordResetConfirmation,
+        db: AsyncSession = Depends(get_db),
+        producer: UserMessageProducer = Depends(get_producer)
+    ):
 
     pyload =  decode_password_reset_token(token=user_data.reset_token)
     email = pyload.get('email', None)
