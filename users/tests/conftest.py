7bb502426269616604f61e369f085bc00963d09f
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-04-08 10:03:02 +0000

    Rewrite fixtures

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -1,31 +1,24 @@
-import asyncio
 import dotenv
 import pytest
-import pytest_asyncio
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from testcontainers.postgres import PostgresContainer
 from testcontainers.core.waiting_utils import wait_for_logs
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 
-dotenv.load_dotenv('.env.test')
+async def lifespan(app):
+    from shared_utils.db.session import init_db, close_db, drop_db
 
-Base = declarative_base()
+    await init_db()
+    
+    yield
+
+    await drop_db()
+    await close_db()
 
 
 @pytest.fixture(scope="session")
-def event_loop():
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope="module")
-def postgres_container(event_loop):
-    # Start a PostgreSQL container with Testcontainers
+def postgres_container():
     postgres = PostgresContainer(
         image="postgres:17.4-alpine",
         driver="asyncpg"
@@ -45,73 +38,32 @@ def postgres_container(event_loop):
     postgres.stop()
 
 
-@pytest_asyncio.fixture(scope='module')
-async def engine(postgres_container):
-
-    engine = create_async_engine(
-        url=postgres_container.get_connection_url()
-    )
-
-    async with engine.begin() as conn:
-        await conn.run_sync(
-            Base.metadata.create_all
-        )
-
-    yield engine
-
-    async with engine.begin() as conn:
-        await conn.run_sync(
-            Base.metadata.drop_all
-        )
-
-    await engine.dispose()
-
-
-@pytest_asyncio.fixture(scope="module")
-async def session_local(engine):
-    session_local = async_sessionmaker(
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-        autocommit=False
-    )
-    return session_local
-
-
-@pytest_asyncio.fixture(scope='function')
-async def get_db(session_local):
-    async with session_local() as session:
-        yield session
-        await session.rollback()  # Ensure clean state between tests
-
-
-@pytest.fixture(scope="module")
-def app(postgres_container, get_db):
+@pytest.fixture(scope="function")
+def app(postgres_container):
     from shared_utils.core.conf import settings
+    
+    dotenv.load_dotenv('.env.test')
 
     settings.SQLALCHEMY_DATABASE_URL = postgres_container.get_connection_url()
 
-    from shared_utils.db.session import get_db as default_get_db
     from app.api.endpoints import auth, users, verification, password
 
-
-    test_app = FastAPI(
+    app = FastAPI(
+        lifespan=lifespan,
         docs_url='/api/users/docs',
         redoc_url='/api/users/redoc',
         openapi_url='/api/users/openapi.json'
     )
 
-    test_app.include_router(auth.auth_router, prefix='/api')
-    test_app.include_router(users.user_router, prefix='/api')
-    test_app.include_router(verification.email_verification_router, prefix='/api')
-    test_app.include_router(password.password_reset_router, prefix='/api')
+    app.include_router(auth.auth_router, prefix='/api')
+    app.include_router(users.user_router, prefix='/api')
+    app.include_router(verification.email_verification_router, prefix='/api')
+    app.include_router(password.password_reset_router, prefix='/api')
 
-    test_app.dependency_overrides[default_get_db] = get_db
-
-    return test_app
+    return app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def client(app):
-    with TestClient(app) as client:
-        yield client
+    with TestClient(app) as test_client:
+        yield test_client
