b92eaa70f5980e46d603afa59b797b5e19d0b94c
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-08 16:18:38 +0200

    Use dependency injection for producer

--- users/app/api/endpoints/users.py
+++ users/app/api/endpoints/users.py
@@ -10,7 +10,8 @@ from app.repositories.user import (create_user, is_user_exist, get_user_by_id, g
                                    update_user, delete_user)
 from app.schemas.user import UserCreate, UserLogin, UserUpdate, UserRetrieve
 from app.api.deps import get_current_user, get_current_admin_user
-from app.producer.api import send_user_creation_message, send_user_email_verification_message
+from app.producer.api import get_producer
+from app.producer.producer import UserMessageProducer
 
 
 user_router = APIRouter(
@@ -20,12 +21,17 @@ user_router = APIRouter(
 
 
 @user_router.get('/me/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
-async def get_user_route(current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+async def get_user_route(current_user: User = Depends(get_current_user)):
     return current_user
 
 
 @user_router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserRetrieve)
-async def create_user_route(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
+async def create_user_route(
+        user_data: UserCreate,
+        db: AsyncSession = Depends(get_db),
+        producer: UserMessageProducer = Depends(get_producer)
+    ):
+
     db_user = await is_user_exist(username=user_data.username, email=user_data.email, db=db)
     if db_user:
         raise HTTPException(
@@ -37,15 +43,24 @@ async def create_user_route(user_data: UserCreate, db: AsyncSession = Depends(ge
 
     ret_user = UserRetrieve.from_orm(db_user)
 
-    await send_user_creation_message(ret_user)
-    await send_user_email_verification_message(ret_user)
+    await producer.send_user_creation_message(
+        message_data=ret_user.model_dump()
+    )
+
+    await producer.send_user_email_verification_message(
+        message_data=ret_user.model_dump()
+    )
 
     return ret_user
 
 
 @user_router.get('/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
-async def get_user_route(user_id: int, current_user: User = Depends(get_current_user),
-                         db: AsyncSession = Depends(get_db)):
+async def get_user_route(
+        user_id: int,
+        current_user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+    ):
+
     user = await get_user_by_id(id=user_id, db=db)
 
     if user:
@@ -64,7 +79,10 @@ async def get_user_route(user_id: int, current_user: User = Depends(get_current_
 
 
 @user_router.get('/', status_code=status.HTTP_200_OK, response_model=List[UserRetrieve])
-async def get_users_route(current_user: User = Depends(get_current_admin_user), db: AsyncSession = Depends(get_db)):
+async def get_users_route(
+        current_user: User = Depends(get_current_admin_user),
+        db: AsyncSession = Depends(get_db)
+    ):
 
     if current_user.is_admin:
         users = await get_all_users(db=db)
@@ -77,10 +95,13 @@ async def get_users_route(current_user: User = Depends(get_current_admin_user), 
 
 
 @user_router.delete('/{user_id}/', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user_route(user_id: int, current_user: User = Depends(get_current_user),
-                            db: AsyncSession = Depends(get_db)):
-    user = await get_user_by_id(id=user_id, db=db)
+async def delete_user_route(
+        user_id: int,
+        current_user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+    ):
 
+    user = await get_user_by_id(id=user_id, db=db)
     if user:
         if current_user.is_admin or current_user.id == user_id:
             await delete_user(user_id=user_id, db=db)
@@ -98,10 +119,14 @@ async def delete_user_route(user_id: int, current_user: User = Depends(get_curre
 
 
 @user_router.put('/{user_id}/', status_code=status.HTTP_200_OK, response_model=UserRetrieve)
-async def update_user_route(user_id: int, user_data: UserUpdate, current_user: User = Depends(get_current_user),
-                      db: AsyncSession = Depends(get_db)):
-    user = await get_user_by_id(id=user_id, db=db)
+async def update_user_route(
+        user_id: int,
+        user_data: UserUpdate,
+        current_user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+    ):
 
+    user = await get_user_by_id(id=user_id, db=db)
     if user:
         if current_user.is_admin or current_user.id == user_id:
             return await update_user(user_id=user_id, user=user_data, db=db)
