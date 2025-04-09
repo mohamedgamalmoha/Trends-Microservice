eee44d415e700662ce4acd291aac9efd332ed75c
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-04-09 21:37:09 +0000

    Restructure conftest file

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -1,27 +1,16 @@
+import json
 import dotenv
 import pytest
+import pytest_asyncio
+import aio_pika
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from testcontainers.postgres import PostgresContainer
-from testcontainers.rabbitmq import RabbitMqContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
-
-class CustomRabbitMqContainer(RabbitMqContainer):
-
-    def get_connection_url(self):
-        return f"amqp://{self.username}:{self.password}@{self.get_container_host_ip()}:{self.get_exposed_port(self.port)}"
-
-
-async def lifespan(app):
-    from shared_utils.db.session import init_db, close_db, drop_db
-
-    await init_db()
-    
-    yield
-
-    await drop_db()
-    await close_db()
+from tests.utils.lifespan import lifespan
+from tests.utils.producer import get_custom_producer
+from tests.utils.rabbitmq import CustomRabbitMqContainer
 
 
 @pytest.fixture(scope="session")
@@ -46,6 +35,44 @@ def postgres_container():
 
 
 @pytest.fixture(scope="session")
+def app_setup_and_teardown(postgres_container):
+    dotenv.load_dotenv('.env.test')
+
+    import os
+    os.environ['SQLALCHEMY_DATABASE_URL'] = postgres_container.get_connection_url()
+
+    yield
+
+
+@pytest.fixture(scope="function")
+def app(app_setup_and_teardown):
+    from app.api.endpoints import auth, users, verification, password
+    from app.producer.api import get_producer
+
+    app = FastAPI(
+        lifespan=lifespan,
+        docs_url='/api/users/docs',
+        redoc_url='/api/users/redoc',
+        openapi_url='/api/users/openapi.json'
+    )
+
+    app.include_router(auth.auth_router, prefix='/api')
+    app.include_router(users.user_router, prefix='/api')
+    app.include_router(verification.email_verification_router, prefix='/api')
+    app.include_router(password.password_reset_router, prefix='/api')
+
+    app.dependency_overrides[get_producer] = get_custom_producer
+
+    return app
+
+
+@pytest.fixture(scope="function")
+def client(app):
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope="session")
 def rabbitmq_container():
     rabbitmq = CustomRabbitMqContainer(
         image="rabbitmq:4.0.6-alpine",
@@ -65,37 +92,67 @@ def rabbitmq_container():
     rabbitmq.stop()
 
 
-@pytest.fixture(scope="function")
-def setup_and_teardown(postgres_container, rabbitmq_container):
+@pytest.fixture(scope="session")
+def producer_setup_and_teardown(rabbitmq_container):
     dotenv.load_dotenv('.env.test')
 
     import os
-    os.environ['SQLALCHEMY_DATABASE_URL'] = postgres_container.get_connection_url()
     os.environ['RABBITMQ_URL'] = rabbitmq_container.get_connection_url()
 
     yield
 
 
-@pytest.fixture(scope="function")
-def app(setup_and_teardown):
-    from app.api.endpoints import auth, users, verification, password
+@pytest_asyncio.fixture
+async def producer(producer_setup_and_teardown):
+    from app.producer.conf import settings, USER_RABBITMQ_EXCHANGE, USER_RABBITMQ_QUEUES
+    from app.producer.producer import UserMessageProducer
 
-    app = FastAPI(
-        lifespan=lifespan,
-        docs_url='/api/users/docs',
-        redoc_url='/api/users/redoc',
-        openapi_url='/api/users/openapi.json'
+    user_producer = UserMessageProducer(
+        url=settings.RABBITMQ_URL,
+        exchange_settings=USER_RABBITMQ_EXCHANGE,
+        queues_settings=USER_RABBITMQ_QUEUES
     )
 
-    app.include_router(auth.auth_router, prefix='/api')
-    app.include_router(users.user_router, prefix='/api')
-    app.include_router(verification.email_verification_router, prefix='/api')
-    app.include_router(password.password_reset_router, prefix='/api')
+    await user_producer.connect()
+    
+    await user_producer.declare_exchange()
+    await user_producer.declare_queues()
 
-    return app
+    yield user_producer
+    
+    await user_producer.close()
 
 
-@pytest.fixture(scope="function")
-def client(app):
-    with TestClient(app) as test_client:
-        yield test_client
+@pytest_asyncio.fixture
+async def consumer_received_messages(producer_setup_and_teardown):
+    from app.producer.conf import settings, USER_CREATION_RABBITMQ_QUEUE, USER_EMAIL_VERIFICATION_RABBITMQ_QUEUE, USER_PASSWORD_FORGET_RABBITMQ_QUEUE
+    
+    connection = await aio_pika.connect_robust(
+        settings.RABBITMQ_URL
+    )
+    channel = await connection.channel()
+    
+    # Dictionary to store received messages by queue
+    received_messages = {
+        USER_CREATION_RABBITMQ_QUEUE.name: [],
+        USER_EMAIL_VERIFICATION_RABBITMQ_QUEUE.name: [],
+        USER_PASSWORD_FORGET_RABBITMQ_QUEUE.name: []
+    }
+    
+    # Create message handlers for each queue
+    async def create_consumer(queue_name):
+        async def message_handler(message):
+            async with message.process():
+                received_messages[queue_name].append(json.loads(message.body.decode()))
+        
+        queue = await channel.get_queue(queue_name)
+        await queue.consume(message_handler)
+        
+    # Set up consumers for each queue
+    for queue_name in received_messages.keys():
+        await create_consumer(queue_name)
+    
+    yield received_messages
+    
+    # Cleanup
+    await connection.close()
