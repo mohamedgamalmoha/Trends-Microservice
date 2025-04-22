598d9fa86a9bfddc07cc836ee5aa65206161ec47
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-22 02:31:09 +0200

    Restructure api files

---
+++ users/app/api/v1/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from app.api.v1.endpoints import auth, user, email_verification, password_reset
+
+
+v1_api_router = APIRouter(
+    prefix='/v1',
+    tags=['v1']
+)
+
+v1_api_router.include_router(auth.auth_router)
+v1_api_router.include_router(user.user_router)
+v1_api_router.include_router(email_verification.email_verification_router)
+v1_api_router.include_router(password_reset.password_reset_router)
