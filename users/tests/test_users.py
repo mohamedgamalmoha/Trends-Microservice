a598e8f977ecded5caa7af087760d5c776f29c1b
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-04-22 15:53:41 +0000

    Adjust tests with new api version

--- users/tests/test_users.py
+++ users/tests/test_users.py
@@ -6,7 +6,7 @@ def test_create_user(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     data = response.json()
 
     # Assert response
@@ -24,7 +24,7 @@ def test_create_user_invalid_email(client):
     user_data["email"] = "invalid_email"
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     data = response.json()
 
     # Assert response
@@ -37,11 +37,11 @@ def test_get_user_token(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
 
     # Assert response
@@ -58,16 +58,16 @@ def test_verify_user_token(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
     access_token = data['access_token']
 
     # Make API request to verify token
-    response = client.get("/api/jwt/verify/", headers={"Authorization": f"Bearer {access_token}"})
+    response = client.get("/api/v1/jwt/verify/", headers={"Authorization": f"Bearer {access_token}"})
 
     # Assert response
     assert response.status_code == 204
@@ -78,16 +78,16 @@ def test_get_user(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
     access_token = data['access_token']
 
     # Make API request to get user
-    response = client.get("/api/users/me/", headers={"Authorization": f"Bearer {access_token}"})
+    response = client.get("/api/v1/users/me/", headers={"Authorization": f"Bearer {access_token}"})
     data = response.json()
 
     # Assert response
@@ -102,16 +102,16 @@ def test_get_user_invalid_token(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
     access_token = data['access_token']
 
     # Make API request to get user with invalid token
-    response = client.get("/api/users/me/", headers={"Authorization": f"Bearer {access_token}invalid"})
+    response = client.get("/api/v1/users/me/", headers={"Authorization": f"Bearer {access_token}invalid"})
     data = response.json()
 
     # Assert response
@@ -124,11 +124,11 @@ def test_get_user_expired_token(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
     access_token = data['access_token']
 
@@ -136,7 +136,7 @@ def test_get_user_expired_token(client):
     expired_token = access_token + "expired"
 
     # Make API request to get user with expired token
-    response = client.get("/api/users/me/", headers={"Authorization": f"Bearer {expired_token}"})
+    response = client.get("/api/v1/users/me/", headers={"Authorization": f"Bearer {expired_token}"})
     data = response.json()
 
     # Assert response
@@ -146,11 +146,11 @@ def test_get_user_expired_token(client):
 
 def test_get_user_unauthorized(client):
     # Make API request to get user without authentication
-    response = client.get("/api/users/me/")
+    response = client.get("/api/v1/users/me/")
     data = response.json()
 
     # Assert response
-    assert response.status_code == 401
+    assert response.status_code == 403
     assert data['detail'] == "Not authenticated"
 
 
@@ -159,16 +159,16 @@ def test_get_user_not_found(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     data = response.json()
     access_token = data['access_token']
 
     # Make API request to get user with invalid ID
-    response = client.get("/api/users/9999999/", headers={"Authorization": f"Bearer {access_token}"})
+    response = client.get("/api/v1/users/9999999/", headers={"Authorization": f"Bearer {access_token}"})
     data = response.json()
 
     # Assert response
@@ -181,18 +181,18 @@ def test_update_user(client):
     user_data = UserCreateFactoryDict()
     
     # Make API request to create user   
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
     user_id = response.json()["id"]
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     assert response.status_code == 200
     data = response.json()
     access_token = data['access_token']     
     
     # Make API request to update user
-    response = client.put(f"/api/users/{user_id}/", headers={"Authorization": f"Bearer {access_token}"}, json={"first_name": "new_first_name"})
+    response = client.put(f"/api/v1/users/{user_id}/", headers={"Authorization": f"Bearer {access_token}"}, json={"first_name": "new_first_name"})
     data = response.json()
     
     # Assert response
@@ -207,19 +207,19 @@ def test_delete_user(client):
     user_data = UserCreateFactoryDict()
 
     # Make API request to create user
-    response = client.post("/api/users/", json=user_data)
+    response = client.post("/api/v1/users/", json=user_data)
     assert response.status_code == 201
     user_id = response.json()["id"]
 
     # Make API request to get user token
-    response = client.post("/api/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
+    response = client.post("/api/v1/jwt/create/", json={"email": user_data["email"], "password": user_data["password"]})
     assert response.status_code == 200
     data = response.json()
     access_token = data['access_token']
 
     # Make API request to delete user
-    response = client.delete(f"/api/users/{user_id}/", headers={"Authorization": f"Bearer {access_token}"})
-    
+    response = client.delete(f"/api/v1/users/{user_id}/", headers={"Authorization": f"Bearer {access_token}"})
+
     # Assert response
     assert response.status_code == 204
     assert response.content == b""
