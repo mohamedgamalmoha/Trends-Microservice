a54d81b45712039a23ee3391a0b35db72a5726f7
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-23 10:13:42 +0200

    Write task repository

--- trends/app/repositories/task.py
+++ trends/app/repositories/task.py
@@ -1,82 +1,125 @@
-from typing import Sequence
-
 from fastapi import Depends
-from sqlalchemy.sql import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from shared_utils.db.session import get_db
+from shared_utils.exceptions import ObjAlreadyExist
+from shared_utils.repository.sqlalchemy import SQLAlchemyModelRepository
 
 from app.models.task import Task
-from app.schemas.task import TaskCreate, TaskRetrieve, TaskUpdate, TrendTaskUpdate
 
 
-async def create_task(task_id: str, task: TaskCreate, db: AsyncSession = Depends(get_db)) -> Task:
+class TaskModelRepository(SQLAlchemyModelRepository[Task]):
+    """
+    TaskModelRepository is a repository class for managing Task instances.
+    It provides methods to create, retrieve, update, and delete Task records
+    in the database using SQLAlchemy ORM.
+    """
+    async def create(self, id: str, user_id: str, q: str, **other_fields) -> Task:
+        """
+        Create a new task.
 
-    db_task = Task(
-        task_id=task_id,
-        **task.model_dump(),
+        Args:
+            - id (str): Task id to create.
+            - username (str): Task user_id to create.
+            - q (str): Task q to create.
+            - **other_fields: Remaining fields to create.
+
+        Returns:
+            - Task: The created task instance.
+
+        Raises:
+            - ObjAlreadyExist: If a task with the same id exists.
+        """
+        is_task_exist = await self.get_by_id(id=id)
+        if is_task_exist:
+            raise ObjAlreadyExist()
+
+        return await super().create(id=id, user_id=user_id, q=q, **other_fields)
+
+    async def get_by_id(self, id: str) -> Task:
+        """
+        Retrieves a single task instance by id.
+
+        Args:
+            - id (str): Task id to search for.
+
+        Returns:
+            - Task: The task instance if found.
+
+        Raises:
+            - ObjDoesNotExist: If no instance is found with the given ID.
+        """
+        return await super().get_by_id(id=id)
+
+    async def get_by_user_id(self, id: str, user_id: str) -> Task:
+        """
+        Get task by task id & user id.
+
+        Args:
+            - id (str): Task id to search for.
+            - user_id (str): User id to search for.
+
+        Returns:
+            - Task: The task instance if found.
+
+        Raises:
+            - ObjDoesNotExist: If no instance is found with the given ID.
+        """
+        await self.filter_by(id=id, user_id=user_id)
+
+    async def update(self, id: str, **kwargs) -> Task:
+        """
+        Update a task instance.
+
+        Args:
+            - id (str): Task id to update.
+            - **kwargs: Fields to update.
+
+        Returns:
+            - Task: The updated task instance.
+        """
+        return await super().update(id=id, **kwargs)
+
+    async def increment_retry_count(self, id: str, increment_by: int = 1) -> Task:
+        """
+        Increment the retry count of a task instance.
+
+        Args:
+            - id (str): Task id to update.
+            - increment_by (int): The amount to increment the retry count by.
+
+        Returns:
+            - Task: The updated task instance.
+
+        Raises:
+            - ObjDoesNotExist: If no instance is found with the given ID.
+        """
+        task = self.get_by_id(id=id)
+        increment_count = task.retry_count + increment_by
+        return await self.update(id=id, retry_count=increment_count)
+
+    async def delete(self, id: str) -> None:
+        """
+        Delete a task instance.
+
+        Aegs:
+            - id (str): Task id to delete.
+
+        Raises:
+            - ObjDoesNotExist: If no instance is found with the given ID.
+        """
+        await super().delete(id=id)
+
+
+def get_task_repository(db: AsyncSession = Depends(get_db)) -> TaskModelRepository:
+    """
+    Dependency to get the TaskModelRepository instance.
+
+    Args:
+        - db (AsyncSession): The database session.
+
+    Returns:
+        - TaskModelRepository: The TaskModelRepository instance.
+    """
+    return TaskModelRepository(
+        db=db
     )
-
-    db.add(db_task)
-    await db.commit()
-    await db.refresh(db_task)
-
-    return db_task
-
-
-async def get_task_by_id(task_id: str, db: AsyncSession = Depends(get_db)) -> Task | None:
-    result = await db.execute(
-        select(Task).filter_by(task_id=task_id)
-    )
-    return result.scalar_one_or_none()
-
-
-async def get_user_task_by_id(user_id: int, task_id: str, db: AsyncSession = Depends(get_db)) -> Task | None:
-    result = await db.execute(
-        select(Task).filter_by(user_id=user_id, task_id=task_id)
-    )
-    return result.scalar_one_or_none()
-
-
-async def get_user_tasks(user_id: int, db: AsyncSession = Depends(get_db)) -> Sequence[Task]:
-    result = await db.execute(
-        select(Task).filter_by(user_id=user_id)
-    )
-    return result.scalars().all()
-
-
-async def get_all_tasks(db: AsyncSession = Depends(get_db)) -> Sequence[Task]:
-    result = await db.execute(
-        select(Task)
-    )
-    return result.scalars().all()
-
-
-async def update_task(task_id: str, task_update: TaskUpdate | TrendTaskUpdate, db: AsyncSession = Depends(get_db)) \
-        -> Task | None:
-
-    db_task = await get_task_by_id(task_id=task_id, db=db)
-
-    if not db_task:
-        return None
-
-    for field_name, new_field_value in task_update.model_dump().items():
-        if new_field_value:
-            setattr(db_task, field_name, new_field_value)
-
-    if isinstance(task_update, TrendTaskUpdate) and task_update.increment_retry_count:
-        db_task.retry_count += 1
-
-    await db.commit()
-    await db.refresh(db_task)
-
-    return db_task
-
-
-async def delete_task(task_id: str, db: AsyncSession = Depends(get_db)) -> None:
-    db_task = await get_task_by_id(task_id=task_id, db=db)
-
-    if not db_task:
-        return None
-
-    await db.delete(db_task)
-    await db.commit()
