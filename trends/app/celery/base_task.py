0b51b412c14a4056c445c0ada362e4f9b12dbf9e
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-23 10:59:30 +0200

    Rwrite event handler using task service

--- trends/app/celery/base_task.py
+++ trends/app/celery/base_task.py
@@ -5,7 +5,7 @@ from shared_utils.db.session import get_db
 from shared_utils.schemas.status import TaskStatus
 from shared_utils.async_handler import AsyncHandler
 
-from app.repositories.task import update_task
+from app.services.task import TaskService, get_task_service
 from app.schemas.task import TrendResponseQListItem, TrendResponse, TrendError, TrendTaskUpdate
 
 
@@ -14,65 +14,45 @@ class TrendTask(Task):
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def before_start(db, task_id, args, kwargs):
-        await update_task(
-            task_id=task_id,
-            task_update=TrendTaskUpdate(
-                status=TaskStatus.IN_PROGRESS
-            ),
-            db=db
+        task_service = TaskService(db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.IN_PROGRESS
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_success(db, retval, task_id, args, kwargs):
-        await update_task(
-            task_id=task_id,
-            task_update=TrendTaskUpdate(
-                status=TaskStatus.COMPLETED,
-                result_data=[
-                    TrendResponse(
-                        date=ret['date'],
-                        is_partial=ret['is_partial'],
-                        q_list=[
-                            TrendResponseQListItem(
-                                query=q['query'],
-                                value=q['value']
-                            )
-                            for q in ret['q_list']
-                        ]
-                    ) for ret in retval
-                ]
-            ),
-            db=db
+        task_service = TaskService(db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.COMPLETED,
+            result_data=retval
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_failure(db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=TrendTaskUpdate(
-                status=TaskStatus.FAILED,
-                error=TrendError(
-                    code=500,
-                    error=traceback.format_exc()
-                )
-            ),
-            db=db
+        task_service = TaskService(db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.FAILED,
+            error={
+                'code': 500,
+                'error': traceback.format_exc()
+            }
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_retry(db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=TrendTaskUpdate(
-                status=TaskStatus.RETRY,
-                error=TrendError(
-                    code=500,
-                    error=traceback.format_exc()
-                ),
-                increment_retry_count=True
-            ),
-            db=db
+        task_service = TaskService(db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.RETRY,
+            error={
+                'code': 500,
+                'error': traceback.format_exc()
+            },
         )
+        await task_service.increment_retry_count(id=task_id, increment_by=1)
