d128f41e8524c3d647b418567f2b0e703caf4802
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-24 14:01:41 +0200

    Rewrite event handler using task service

--- thinker/app/celery/base_task.py
+++ thinker/app/celery/base_task.py
@@ -1,11 +1,12 @@
 import traceback
 
 from celery import Task
+from celery.bin.control import status
 from shared_utils.db.session import get_db
 from shared_utils.schemas.status import TaskStatus
 from shared_utils.async_handler import AsyncHandler
 
-from app.repositories.task import update_task
+from app.services.task import TaskService, get_task_service
 from app.schemas.task import ThinkResponse, ThinkError, ThinkTaskUpdate
 
 
@@ -14,55 +15,45 @@ class ThinkTask(Task):
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def before_start(db, task_id, args, kwargs):
-        await update_task(
-            task_id=task_id,
-            task_update=ThinkTaskUpdate(
-                status=TaskStatus.IN_PROGRESS
-            ),
-            db=db
+        task_service = TaskService(db=db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.IN_PROGRESS
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_success(db, retval, task_id, args, kwargs):
-        await update_task(
-            task_id=task_id,
-            task_update=ThinkTaskUpdate(
-                status=TaskStatus.COMPLETED,
-                result_data=ThinkResponse(
-                    ** retval
-                )
-            ),
-            db=db
+        task_service = TaskService(db=db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.COMPLETED,
+            result_data=retval
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_failure(db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=ThinkTaskUpdate(
-                status=TaskStatus.FAILED,
-                error=ThinkError(
-                    code=500,
-                    error=traceback.format_exc()
-                )
-            ),
-            db=db
+        task_service = TaskService(db=db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.FAILED,
+            error={
+                'code': 500,
+                'error': traceback.format_exc()
+            }
         )
 
     @staticmethod
     @AsyncHandler.with_async_generator(get_db)
     async def on_retry(db, exc, task_id, args, kwargs, einfo):
-        await update_task(
-            task_id=task_id,
-            task_update=ThinkTaskUpdate(
-                status=TaskStatus.RETRY,
-                error=ThinkError(
-                    code=500,
-                    error=traceback.format_exc()
-                ),
-                increment_retry_count=True
-            ),
-            db=db
+        task_service = TaskService(db=db)
+        await task_service.update(
+            id=task_id,
+            status=TaskStatus.RETRY,
+            error={
+                'code': 500,
+                'error': traceback.format_exc()
+            }
         )
+        await task_service.increment_retry_count(id=task_id, increment_by=1)
