6d892b1187213d9c5c9307c56d64897742005933
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-05-09 22:04:20 +0000

    Update users settings

--- users/app/core/conf.py
+++ users/app/core/conf.py
@@ -1,5 +1,5 @@
 import os
-import logging
+from typing import Optional
 
 import dotenv
 from pydantic_settings import BaseSettings
@@ -7,30 +7,32 @@ from pydantic_settings import BaseSettings
 
 dotenv.load_dotenv('../../.env')
 
-logging.basicConfig(level=logging.INFO)
-
 
 class Settings(BaseSettings):
     # General
-    SQLALCHEMY_DATABASE_URL: str= os.environ.get('SQLALCHEMY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URL: Optional[str] = os.environ.get('SQLALCHEMY_DATABASE_URL', None)
 
     # Password Envs
-    PASSWORD_CRYPT_CONTEXT_SCHEMA: str = os.environ.get("PASSWORD_CRYPT_CONTEXT_SCHEMA")
+    PASSWORD_CRYPT_CONTEXT_SCHEMA: Optional[str] = os.environ.get("PASSWORD_CRYPT_CONTEXT_SCHEMA", None)
 
     # Access Token Envs
-    ACCESS_TOKEN_SECRET_KEY: str = os.environ.get("ACCESS_TOKEN_SECRET_KEY")
-    ACCESS_TOKEN_ALGORITHM: str = os.environ.get("ACCESS_TOKEN_ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ACCESS_TOKEN_SECRET_KEY: Optional[str] = os.environ.get("ACCESS_TOKEN_SECRET_KEY", None)
+    ACCESS_TOKEN_ALGORITHM: Optional[str] = os.environ.get("ACCESS_TOKEN_ALGORITHM", None)
+    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[int] = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", None)
 
     # Verification Token Envs
-    VERIFICATION_TOKEN_SECRET_KEY: str = os.environ.get("VERIFICATION_TOKEN_SECRET_KEY")
-    VERIFICATION_TOKEN_ALGORITHM: str = os.environ.get("VERIFICATION_TOKEN_ALGORITHM")
-    VERIFICATION_TOKEN_EXPIRE_MINUTES: int = os.environ.get("VERIFICATION_TOKEN_EXPIRE_MINUTES")
+    VERIFICATION_TOKEN_SECRET_KEY: Optional[str] = os.environ.get("VERIFICATION_TOKEN_SECRET_KEY", None)
+    VERIFICATION_TOKEN_ALGORITHM: Optional[str] = os.environ.get("VERIFICATION_TOKEN_ALGORITHM", None)
+    VERIFICATION_TOKEN_EXPIRE_MINUTES: Optional[int] = os.environ.get("VERIFICATION_TOKEN_EXPIRE_MINUTES", None)
 
     # Password Reset Token Envs
-    PASSWORD_REST_TOKEN_SECRET_KEY: str = os.environ.get("PASSWORD_REST_TOKEN_SECRET_KEY")
-    PASSWORD_REST_TOKEN_ALGORITHM: str = os.environ.get("PASSWORD_REST_TOKEN_ALGORITHM")
-    PASSWORD_REST_TOKEN_EXPIRE_MINUTES: int = os.environ.get("PASSWORD_REST_TOKEN_EXPIRE_MINUTES")
+    PASSWORD_REST_TOKEN_SECRET_KEY: Optional[str] = os.environ.get("PASSWORD_REST_TOKEN_SECRET_KEY", None)
+    PASSWORD_REST_TOKEN_ALGORITHM: Optional[str] = os.environ.get("PASSWORD_REST_TOKEN_ALGORITHM", None)
+    PASSWORD_REST_TOKEN_EXPIRE_MINUTES: Optional[int] = os.environ.get("PASSWORD_REST_TOKEN_EXPIRE_MINUTES", None)
+
+    class Config:
+        frozen = True
+        case_sensitive = True
 
 
 settings = Settings()
