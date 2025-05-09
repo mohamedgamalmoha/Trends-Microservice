b1e71b323d2fe819ba18ee25ce972803381b95eb
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-05-09 22:08:22 +0000

    Update thinker settings

--- thinker/app/core/conf.py
+++ thinker/app/core/conf.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 import dotenv
 from pydantic_settings import BaseSettings
@@ -8,17 +9,21 @@ dotenv.load_dotenv('../../.env')
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URL: Optional[str] = os.environ.get('SQLALCHEMY_DATABASE_URL', None)
    
-    USER_AUTH_URL: str = os.environ.get('USER_AUTH_URL')
-    USER_INFO_URL: str = os.environ.get('USER_INFO_URL')
+    USER_AUTH_URL: Optional[str] = os.environ.get('USER_AUTH_URL', None)
+    USER_INFO_URL: Optional[str] = os.environ.get('USER_INFO_URL', None)
    
-    OLLAMA_API_URL: str = os.environ.get('OLLAMA_API_URL')
-    OLLAMA_MODEL_NAME: str = os.environ.get('OLLAMA_MODEL_NAME')
-    OLLAMA_REQUEST_TIMEOUT: int = os.environ.get('OLLAMA_REQUEST_TIMEOUT')
+    OLLAMA_API_URL: Optional[str] = os.environ.get('OLLAMA_API_URL', None)
+    OLLAMA_MODEL_NAME: Optional[str] = os.environ.get('OLLAMA_MODEL_NAME', None)
+    OLLAMA_REQUEST_TIMEOUT: int = os.environ.get('OLLAMA_REQUEST_TIMEOUT', None)
 
-    CELERY_BROKER_URL: str = os.environ.get('CELERY_BROKER_URL')
-    CELERY_RESULT_BACKEND: str = os.environ.get('CELERY_RESULT_BACKEND')
+    CELERY_BROKER_URL: Optional[str] = os.environ.get('CELERY_BROKER_URL', None)
+    CELERY_RESULT_BACKEND: Optional[str] = os.environ.get('CELERY_RESULT_BACKEND', None)
+
+    class Config:
+        frozen = True
+        case_sensitive = True
 
 
 settings = Settings()
