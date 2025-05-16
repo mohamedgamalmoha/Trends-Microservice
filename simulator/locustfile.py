d74af619891ad7484f0623e94824edcf35176855
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-16 16:55:46 +0300

    Add user for each service

--- simulator/locustfile.py
+++ simulator/locustfile.py
@@ -1,14 +1,32 @@
 from locust import FastHttpUser, between
 
-from locustfiles.conf import settings
-from locustfiles.users.tasks import RegularUserTasks
+from locustfiles.comman.conf import settings
+from locustfiles.users.tasks import UserTasks
 from locustfiles.trends.tasks import TrendsTasks
 from locustfiles.thinker.tasks import ThinkerTasks
 
 
-class RegularUser(FastHttpUser):
+class User(FastHttpUser):
     host = settings.USER_SERVICE_URL
-    tasks = [RegularUserTasks, TrendsTasks, ThinkerTasks]
+    tasks = [UserTasks]
+    wait_time = between(
+        min_wait=settings.MIN_WAITING_TIME,
+        max_wait=settings.MAX_WAITING_TIME
+    )
+
+
+class TrendsUser(FastHttpUser):
+    host = settings.TRENDS_SERVICE_URL
+    tasks = [TrendsTasks]
+    wait_time = between(
+        min_wait=settings.MIN_WAITING_TIME,
+        max_wait=settings.MAX_WAITING_TIME
+    )
+
+
+class ThinkerUser(FastHttpUser):
+    host = settings.USER_SERVICE_URL
+    tasks = [ThinkerTasks]
     wait_time = between(
         min_wait=settings.MIN_WAITING_TIME,
         max_wait=settings.MAX_WAITING_TIME
