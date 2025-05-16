cdb200c106d4d4fa91cd59f82debb840b6fc9731
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-16 08:15:40 +0300

    Add more tasks to regular user

--- simulator/locustfile.py
+++ simulator/locustfile.py
@@ -2,11 +2,13 @@ from locust import FastHttpUser, between
 
 from locustfiles.conf import settings
 from locustfiles.users.tasks import RegularUserTasks
+from locustfiles.trends.tasks import TrendsTasks
+from locustfiles.thinker.tasks import ThinkerTasks
 
 
 class RegularUser(FastHttpUser):
     host = settings.USER_SERVICE_URL
-    tasks = [RegularUserTasks]
+    tasks = [RegularUserTasks, TrendsTasks, ThinkerTasks]
     wait_time = between(
         min_wait=settings.MIN_WAITING_TIME,
         max_wait=settings.MAX_WAITING_TIME
