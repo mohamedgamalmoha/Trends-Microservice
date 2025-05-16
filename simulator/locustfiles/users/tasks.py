29374a3c5b2b124c3b9ef6bfd426972a297bec6a
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-16 16:24:24 +0300

    Use auth mixin from comman.auth

--- simulator/locustfiles/users/tasks.py
+++ simulator/locustfiles/users/tasks.py
@@ -2,77 +2,19 @@ import json
 
 from locust import SequentialTaskSet, task
 
-from locustfiles.comman.user import User, UserCreateFactory
+from locustfiles.comman.user import User
+from locustfiles.comman.auth import AuthTaskMixin
 from locustfiles.users.factories import UserUpdateFactory
 
 
-class RegularUserTasks(SequentialTaskSet):
+class RegularUserTasks(AuthTaskMixin, SequentialTaskSet):
 
     def __init__(self, parent):
         super().__init__(parent)
         self.current_user: User = None
         self.auth_token: str = None
 
-    def get_headers(self):
-        return {"Authorization": f"Bearer {self.auth_token}"}
-
-    @task(1)
-    def create(self):
-        user_data = UserCreateFactory.build()
-
-        with self.client.post(
-                "/api/v1/users/",
-                json=user_data,
-                catch_response=True
-        ) as response:
-            if response.status_code == 200:
-                try:
-                    response_data = json.loads(response.text)
-                    user_data.update(response_data)
-                    user_data.pop('password_confirm', None)
-                    self.current_user = User(**user_data)
-                    response.success()
-                except json.JSONDecodeError:
-                    response.failure("Create new user has invalid json schema")
-                    self.interrupt()
-            else:
-                response.failure(
-                    f"Invalid user creation with status:{response.status_code}, and response: {response.text}")
-                self.interrupt()
-
     @task(2)
-    def login(self):
-        # Ensure current_user is not None before proceeding
-        if self.current_user is None:
-            self.interrupt()
-            return
-
-        with self.client.post(
-                "/api/v1/jwt/create/",
-                json={
-                    "email": self.current_user.email,
-                    "password": self.current_user.password
-                },
-                catch_response=True
-        ) as response:
-            if response.status_code == 200:
-                try:
-                    response_data = json.loads(response.text)
-                    self.auth_token = response_data.get("token")
-
-                    if not self.auth_token:
-                        response.failure("Login succeeded but no auth token in response")
-                        self.interrupt()
-                    else:
-                        response.success()
-                except json.JSONDecodeError:
-                    response.failure("Login response not valid JSON")
-                    self.interrupt()
-            else:
-                response.failure(f"Login failed with status code: {response.status_code}")
-                self.interrupt()
-
-    @task(3)
     def get_me(self):
         # Ensure auth_token is not None before proceeding
         if self.auth_token is None:
@@ -81,14 +23,15 @@ class RegularUserTasks(SequentialTaskSet):
 
         with self.client.get(
                 "/api/v1/users/me/",
-                headers=self.get_headers(),
+                headers=self.get_auth_headers(),
                 catch_response=True
         ) as response:
             if response.status_code == 200:
                 response.success()
             else:
                 response.failure(
-                    f"Invalid get my account with status: {response.status_code}, and with response: {response.text}")
+                    f"Invalid get my account with status: {response.status_code}, and with response: {response.text}"
+                )
                 self.interrupt()
 
     @task(4)
@@ -100,7 +43,7 @@ class RegularUserTasks(SequentialTaskSet):
 
         with self.client.get(
                 f"/api/v1/users/{self.current_user.id}/",
-                headers=self.get_headers(),
+                headers=self.get_auth_headers(),
                 catch_response=True
         ) as response:
             if response.status_code == 200:
@@ -110,12 +53,12 @@ class RegularUserTasks(SequentialTaskSet):
                     f"Invalid get my account with status: {response.status_code}, and with response: {response.text}")
                 self.interrupt()
 
-    @task(5)
+    @task(6)
     def update(self):
         update_data = UserUpdateFactory.build()
         with self.client.put(
                 f"/api/v1/users/{self.current_user.id}/",
-                headers=self.get_headers(),
+                headers=self.get_auth_headers(),
                 json=update_data,
                 catch_response=True
         ) as response:
@@ -127,5 +70,6 @@ class RegularUserTasks(SequentialTaskSet):
                 response.success()
             else:
                 response.failure(
-                    f"Invalid get my account with status: {response.status_code}, and with response: {response.text}")
+                    f"Invalid get my account with status: {response.status_code}, and with response: {response.text}"
+                )
                 self.interrupt()
