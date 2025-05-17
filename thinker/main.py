7bd2f83da70fad4b549b9d7c7ba87a5924b4441a
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-17 14:37:52 +0300

    Resolve router import issue

--- thinker/main.py
+++ thinker/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 from shared_utils.core.lifespan import lifespan
 
-from app.api.v1.router import v1_api_router
+from app.api.v1 import v1_api_router
 
 
 app = FastAPI(
