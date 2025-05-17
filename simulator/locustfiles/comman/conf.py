ec24567eb0fe9c7d65f657862674384d28056f14
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-17 14:14:15 +0300

    Add more settings fields

--- simulator/locustfiles/comman/conf.py
+++ simulator/locustfiles/comman/conf.py
@@ -10,8 +10,14 @@ dotenv.load_dotenv('../../.env')
 
 class Settings(BaseSettings):
     SERVICE_BASE_URL: Optional[str] = os.environ.get('SERVICE_BASE_URL', None)
+
+    USER_SERVICE_CREATE_PATH: Optional[str] =  os.environ.get('USER_SERVICE_CREATE_PATH', None)
+    USER_SERVICE_AUTH_PATH: Optional[str] =  os.environ.get('USER_SERVICE_AUTH_PATH', None)
+    USER_SERVICE_AUTH_TOKEN_IDENTIFIER: Optional[str] =  os.environ.get('USER_SERVICE_AUTH_TOKEN_IDENTIFIER', "token")
+
     TRENDS_SERVICE_PATH: Optional[str] = os.environ.get('TRENDS_SERVICE_PATH', None)
     THINK_SERVICE_PATH: Optional[str] = os.environ.get('THINK_SERVICE_PATH', None)
+
     MIN_WAITING_TIME: Optional[int] = os.environ.get('MIN_WAITING_TIME', 1)
     MAX_WAITING_TIME: Optional[int] = os.environ.get('MAX_WAITING_TIME', 5)
 
