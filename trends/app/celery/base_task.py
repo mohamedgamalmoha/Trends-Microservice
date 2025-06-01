57ef369082a4f63dab14ab7e2c9b134bc37cc47f
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-06-01 19:48:46 +0000

    Rewrite base task class

--- trends/app/celery/base_task.py
+++ trends/app/celery/base_task.py
@@ -1,74 +1,130 @@
 import traceback
+from typing import Dict, Any
 
+import requests
 from celery import Task
-from sqlalchemy.ext.asyncio import AsyncSession
-from shared_utils.db.session import get_db
+from billiard.einfo import ExceptionInfo
 from shared_utils.schemas.status import TaskStatus
-from shared_utils.async_handler import AsyncHandler
 
-from app.repositories.task import get_task_repository
-from app.services.task import TaskService, get_task_service
-
-
-def get_task_service_from_db(db: AsyncSession) -> TaskService:
-    """
-    Get task service from db.
-   
-    Args:
-        - db (AsyncSession): Database session.
-    
-    Returns:
-        - TaskService: Task service instance.
-    """
-    task_repository = get_task_repository(db=db)
-    task_service = get_task_service(task_repository=task_repository)
-    return task_service
+from app.core.conf import settings
+from app.core.security import create_task_signature
 
 
 class TrendTask(Task):
 
     @staticmethod
-    @AsyncHandler.with_async_generator(get_db)
-    async def before_start(db, task_id, args, kwargs):
-        task_service = get_task_service_from_db(db)
-        await task_service.update(
-            id=task_id,
-            status=TaskStatus.IN_PROGRESS
+    def _get_request_headers(task_id: str) -> Dict[str, str]:
+        """
+        Helper method to get the headers for the request.
+        
+        Args:
+            - task_id (str): Unique id of the task.
+        
+        Returns:
+            - Dict: Headers for the request.
+        """
+        signature = create_task_signature(message=task_id)
+        return {
+            "Content-Type": "application/json",
+            "X-Signature": signature
+        }
+
+    def _send_request(self, task_id: str, pyload: Dict[str, Any]) -> None:
+        """
+        Helper method to send a request to the task service.
+
+        Args:
+            - task_id (str): Unique id of the task.
+            - pyload (Dict): Data to be sent in the request.
+        """
+        requests.put(
+            f'{settings.TASK_CALLBACK_URL}/task/{task_id}/callback/',
+            headers=self._get_request_headers(task_id),
+            json=pyload
         )
 
-    @staticmethod
-    @AsyncHandler.with_async_generator(get_db)
-    async def on_success(db, retval, task_id, args, kwargs):
-        task_service = get_task_service_from_db(db)
-        await task_service.update(
-            id=task_id,
-            status=TaskStatus.COMPLETED,
-            result_data=retval
-        )
+    def before_start(self, task_id: str, args: tuple, kwargs: dict) -> None:
+        """
+        Handler called before the task starts.
 
-    @staticmethod
-    @AsyncHandler.with_async_generator(get_db)
-    async def on_failure(db, exc, task_id, args, kwargs, einfo):
-        task_service = get_task_service_from_db(db)
-        await task_service.update(
-            id=task_id,
-            status=TaskStatus.FAILED,
-            error={
-                'code': 500,
-                'error': traceback.format_exc()
+        Args:
+            - task_id (str): Unique id of the task.
+            - args (Tuple): Original Args for the task.
+            - kwargs (Dict): Original keyword Args for the task.
+        """
+        self._send_request(
+            task_id=task_id,
+            pyload={
+                'status': TaskStatus.IN_PROGRESS.value,
+                'increment_retry_count': True,
             }
         )
 
-    @staticmethod
-    @AsyncHandler.with_async_generator(get_db)
-    async def on_retry(db, exc, task_id, args, kwargs, einfo):
-        task_service = get_task_service_from_db(db)
-        await task_service.update(
-            id=task_id,
-            status=TaskStatus.RETRY,
-            error={
-                'code': 500,
-                'error': traceback.format_exc()
-            },
+    def on_retry(self, exc: Exception, task_id: str, args: tuple, kwargs: dict, einfo: ExceptionInfo) -> None:
+        """
+        Retry handler.
+
+        This is run by the worker when the task is retried.
+
+        Args:
+            - exc (Exception): The exception raised by the task.
+            - task_id (str): Unique id of the retried task.
+            - args (Tuple): Original Args for the task that is being retried.
+            - kwargs (Dict): Original keyword Args for the task that is being retried.
+            - einfo (ExceptionInfo): Exception information.
+        """
+        self._send_request(
+            task_id=task_id,
+            pyload={
+                'status': TaskStatus.RETRY.value,
+                'error': {
+                    'code': 500,
+                    'error': traceback.format_exc()
+                },
+                'increment_retry_count': True
+            }
         )
-        await task_service.increment_retry_count(id=task_id, increment_by=1)
+
+    def on_success(self, retval: Any, task_id: str, args: tuple, kwargs: dict) -> None:
+        """
+        Success handler.
+
+        This is run by the worker when the task completes successfully.
+
+        Args:
+            - retval (Any): The return value of the task.
+            - task_id (str): Unique id of the completed task.
+            - args (Tuple): Original Args for the task that completed.
+            - kwargs (Dict): Original keyword Args for the task that completed.
+        """
+        self._send_request(
+            task_id=task_id,
+            pyload={
+                'status': TaskStatus.COMPLETED.value,
+                'result_data': retval
+            }
+        )
+
+    def on_failure(self, exc: Exception, task_id: str, args: tuple, kwargs: dict, einfo: ExceptionInfo) -> None:
+        """
+        Error handler.
+
+        This is run by the worker when the task fails.
+
+        Args:
+            - exc (Exception): The exception raised by the task.
+            - task_id (str): Unique id of the failed task.
+            - args (Tuple): Original Args for the task that failed.
+            - kwargs (Dict): Original keyword Args for the task that failed.
+            - einfo (ExceptionInfo): Exception information.
+        """
+        self._send_request(
+            task_id=task_id,
+            pyload={
+                'status': TaskStatus.FAILED.value,
+                'error': {
+                    'code': 500,
+                    'error': traceback.format_exc()
+                }
+            }
+        )
