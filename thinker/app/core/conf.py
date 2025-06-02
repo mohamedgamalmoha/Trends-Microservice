c5e8cfb7f86bcfb675443b2dcb8cc032cce7097b
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2025-06-02 20:23:53 +0000

    Add more fields at settings

--- thinker/app/core/conf.py
+++ thinker/app/core/conf.py
@@ -9,6 +9,11 @@ dotenv.load_dotenv('../../.env')
 
 
 class Settings(BaseSettings):
+    # General
+    SERVICE_NAME: str = os.environ.get('SERVICE_NAME', None)
+    SERVICE_VERSION: str = os.environ.get('SERVICE_VERSION', None)
+
+    # Database Envs
     SQLALCHEMY_DATABASE_URL: Optional[str] = os.environ.get('SQLALCHEMY_DATABASE_URL', None)
    
     USER_AUTH_URL: Optional[str] = os.environ.get('USER_AUTH_URL', None)
@@ -23,6 +28,10 @@ class Settings(BaseSettings):
 
     TASK_CALLBACK_URL: Optional[str] = os.environ.get('TASK_CALLBACK_URL', None)
     TASK_SIGNATURE_KEY: Optional[str] = os.environ.get('TASK_SIGNATURE_KEY', None)
+
+    # OpenTelemetry Envs
+    OTEL_EXPORTER_OTLP_ENDPOINT: Optional[str] = os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT", None)
+    OTEL_EXPORTER_OTLP_INSECURE: Optional[bool] = os.environ.get("OTEL_EXPORTER_OTLP_INSECURE", None)
 
     class Config:
         frozen = True
